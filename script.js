5a6202d9302e8f0eb7a84f84d7e0e9cc7a338860
leoantunes99 <leonardo_antunes_s@hotmail.com>
2025-10-04 15:51:29 -0300

    Adicionei animações no scroll

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// ===== SCROLL ANIMATION =====
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => myObserver.observe(el));
+
 // ===== MENU MOBILE TOGGLE =====
 const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
